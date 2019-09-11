3a16ce9af3a6c77b000e741df72cbb53ff29c013
paulcsc130 <55105838+paulcsc130@users.noreply.github.com>
2019-09-11 15:35:04 -0700

    Create StackADT.java

---
+++ StackADT.java
@@ -0,0 +1,18 @@
+//Nothing Changed
+package itu.data.structure.stack;
+
+public interface StackADT<T> {
+
+	public void push(T element);
+	
+	public T pop();
+	
+	public T peek();
+	
+	public boolean isEmpty();
+	
+	public int size();
+	
+	public String toString();
+	
+}
